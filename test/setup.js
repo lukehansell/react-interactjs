31310261cd76937c6e78603a9d5e59c685d61549
Luke Hansell <luke.hansell@holidayextras.com>
2016-05-06 14:48:04 +0100

    adding jsdom for full mount testing

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,24 @@
+import { jsdom } from 'jsdom'
+
 global.expect = require('chai')
 	.use(require('sinon-chai'))
 	.expect
+
+const exposedProperties = [
+	'window',
+	'navigator',
+	'document'
+];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
